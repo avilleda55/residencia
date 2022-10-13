642a4fdf345ac9f8641e672993283e51d097cb4f
avilleda55 <villedarmzabner@gmail.com>
2022-10-13 15:32:26 -0500

    Agregar modelo Categorias

--- src/main/java/com/sales/point/models/Categoria.java
+++ src/main/java/com/sales/point/models/Categoria.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import lombok.*;
@@ -24,5 +25,8 @@ public class Categoria implements Serializable{
 
     @Column(name = "nombre")
     private String nombreCategoria;
+
+    @OneToOne(mappedBy = "CATEGORIA")
+    private Productos user;
     
 }
